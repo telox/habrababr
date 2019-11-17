8a36548c068f88ce1f1fc9fc8cc46b0c05c8e15e
telox <stepanovu174@gmail.com>
2019-11-17 20:38:17 +0500

    Django Fisrt Comm

---
+++ habr/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=50)
+	short_description = models.TextField(max_length=150)
+	description = models.TextField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	published_date = models.DateTimeField(blank=True, null=True)
+	#rating = 
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
+class Comment(models.Model):
+	description = models.TextField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	published_date = models.DateTimeField(blank=True, null=True)
+	#rating = 
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
