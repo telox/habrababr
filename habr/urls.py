1d83c642349949f9f341b4698c6799853a2b46d2
telox <stepanovu174@gmail.com>
2019-11-18 22:53:15 +0500

    Add reg

--- habr/urls.py
+++ habr/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:id>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:id>/edit/', views.post_edit, name='post_edit'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
