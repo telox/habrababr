6c868eeff9de597942420651615acc4ed8adcd96
telox <stepanovu174@gmail.com>
2019-11-18 20:31:57 +0500

    Add post_detail. Update urls_id

--- habr/views.py
+++ habr/views.py
@@ -4,10 +4,10 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-	posts = Post.objects.get(pk=pk)
+	posts = Post.objects.all()
 	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'habr/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
     return render(request, 'habr/post_detail.html', {'post': post})
